3a599c653c9b3c3ded08ad89df6a07f06d0257f5
Dragon75241 <166435424+Dragon75241@users.noreply.github.com>
2024-05-03 19:55:26 +0000

    antidisestablishmentarianism

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,10 +2,10 @@ import "./../styles/AboutMe.css";
 
 const AboutMe = () => {
   return (
-    <div className="aboutdiv">
+    <div className="aboutdiv p-8">
       <div id="text">
         <h1>Hello!</h1>
-        <p>I&apos;m an ameteur web dev and programmer!</p>
+        <p>I&apos;m an ameteur web developer and programmer!</p>
       </div>
       <div className="face" />
     </div>
