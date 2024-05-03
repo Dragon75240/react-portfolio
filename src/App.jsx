7463e4f12cfa7dc3cbfbcb023cfb0886f1fd236c
Dragon75241 <166435424+Dragon75241@users.noreply.github.com>
2024-05-03 16:10:05 +0000

    update stuff, forgor to commit :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AboutMe from './Pages/AboutMe.jsx';
-import './styles/App.css';
-import Menubar from './Menubar.jsx';
-import Home from './Pages/Home.jsx';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutMe from "./pages/AboutMe.jsx";
+import "./styles/App.css";
+import Menubar from "./Menubar.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
- return (
-   <Router>
-      <Menubar/>
+  return (
+    <Router>
+      <Menubar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutMe />} />
       </Routes>
     </Router>
- );
+  );
 };
 
 export default App;
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,15 @@
+import "./../styles/AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="aboutdiv">
+      <div id="text">
+        <h1>Hello!</h1>
+        <p>I&apos;m an ameteur web dev and programmer!</p>
+      </div>
+      <div className="face" />
+    </div>
+  );
+};
+
+export default AboutMe;
